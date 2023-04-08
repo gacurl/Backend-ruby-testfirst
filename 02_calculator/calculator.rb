4497ff09731a3f5b6019dc620679aa1a7f2d271d
gacurl <greg.a.curl@gmail.com>
2023-04-08 15:34:55 -0400

    solve q2 and q3

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(array)
+  output = 0
+  array.each do |item|
+    output += item
+  end
+  output
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,14 @@
 #write your code here
+def echo(echoed)
+  echoed
+end
+
+def shout(shouted)
+  shouted.upcase
+end
+
+def repeat(word, num_of_times=2)
+  #make it an array (via .map) since I kept getting the trailing space
+  #internet helped with this one
+  num_of_times.times.map { word + '' }.join(' ')
+end
