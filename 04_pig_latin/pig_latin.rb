806b21280032d5a2494a41bb676610f31e7ca3f5
gacurl <greg.a.curl@gmail.com>
2023-04-11 19:46:27 -0400

    solve pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,31 @@
 #write your code here
+def translate(word)
+	translation = []
+	word.split(' ').map do |word|
+		translation << translate_word(word)
+	end
+	translation.join(' ')
+end
+
+def translate_word(string)
+	word = string.split('')
+	trans = []
+	x = word.dup
+
+	if word[0..2].join == 'squ'
+		trans << word[0..2]
+		x.shift(3)
+	elsif word[0] == 'q'
+		trans << word[0..1]
+		x.shift(2)
+	else
+		word.map do |letter|
+			break if %w(a e i o u).include?(letter)
+				trans << letter
+				x.shift
+		end
+	end
+	x.join + trans.join + "ay"
+end
+
+puts translate("apple")
